818e0f4d6185070bb816603973d898cfec5eeb67
edison <arlicle@gmail.com>
2019-02-16 16:44:08 +0800

    Fix *

--- project.clj
+++ project.clj
@@ -4,5 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/tools.namespace "0.3.0-alpha4"]
+                 ]
   ;:plugins [[lein-laniu-task "0.1.0"]]
   )
--- src/leiningen/make_migrations.clj
+++ src/leiningen/make_migrations.clj
@@ -1,6 +1,76 @@
-(ns leiningen.make-migrations)
+(ns leiningen.make-migrations
+  (:require [clojure.java.io :as io]
+            [clojure.tools.namespace.find :as ns-find]
+            [clojure.tools.namespace.file :as ns-file]
+            ))
+
+
+
+;
+;(def p ["/code/clojure/test-app/src/"])
+;(def a
+;  (filter
+;    #(= "models.clj" (.getName %))
+;    (apply clojure.set/union
+;           (map (comp #(ns-find/find-sources-in-dir % ns-find/clj)
+;                      io/file)
+;                p))))
+;
+;
+;(map (comp #(ns-find/find-sources-in-dir % ns-find/clj)
+;           io/file)
+;     p)
+;
+;(load-file (str (first a)))
+;
+;(doseq [f a]
+;  (println (.getName f)))
 
 (defn make-migrations
   "I don't do a lot."
   [project & args]
+  (let [aaa
+        (->> (all-ns)
+             (map ns-name)
+             (map name)
+             (filter #(clojure.string/ends-with? % ".models")))
+
+        ;source-files (apply clojure.set/union
+        ;                    (map (comp #(ns-find/find-sources-in-dir % ns-find/clj)
+        ;                               io/file)
+        ;                         (project :source-paths)))
+        ;model-ns-names (->> source-files
+        ;                    (map (comp second ns-file/read-file-ns-decl))
+        ;                    (filter #(clojure.string/ends-with? (str %) ".models"))
+        ;                    )
+
+        ;cccc (apply clojure.set/union
+        ;            (map (comp second ns-file/read-file-ns-decl)
+        ;                 (project :source-paths)))
+        ]
+
+    (load-file "/code/clojure/test-app/src/test_app/models.clj")
+
+    ;(println cccc)
+    ;(println aaa)
+    ;(println source-files)
+    ;(println model-ns-names)
+
+    ;(doseq [a-ns-name model-ns-names]
+    ;  (require (list a-ns-name))
+    ;  (println (ns-interns a-ns-name))
+    ;  )
+    ;(doseq [f source-files]
+    ;  (load-file (str f )))
+    ;(require ['test-app.models])
+
+    )
+
+  (println (:source-paths project))
+  (println args)
   (println "Hello makemigrations!"))
+
+(defn get-var-namespace
+  [qualified-var]
+  {:pre [(var? qualified-var)]}
+  (-> qualified-var meta :ns))
